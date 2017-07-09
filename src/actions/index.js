40a3faf197b93ea600e551ca0661eb1137424ea4
intuitional <intuitional@users.noreply.github.com>
2017-07-10 02:14:28 +1000

    lect143

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POSTS = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
-const MY_KEY = '?key=OCTOPUSHORSEKING';
+const MY_KEY = '?key=PAPERCLIP1432';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${MY_KEY}`);
@@ -13,4 +14,14 @@ export function fetchPosts() {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${MY_KEY}`, values)
+        .then(() => callback());
+
+    return {
+        type: CREATE_POSTS,
+        payload: request
+    }
 }
